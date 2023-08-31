966bd28684c5d602e1e312da219aae46efbb1608
abdulhay103 <abdulhay.me@gmail.com>
2023-08-31 23:43:03 +0600

    done

--- src/app/api/about/route.js
+++ src/app/api/about/route.js
@@ -2,5 +2,7 @@ import { NextResponse } from "next/server";
 import { headers } from "next/headers";
 
 export async function POST(req, res) {
-  return NextResponse.json({ msg: "ok" });
+  const allHeader = headers();
+  let connection = allHeader.get("Connection");
+  return NextResponse.json({ msg: connection }, { status: 402 });
 }
