908672e3063a4493f42a92c6d9550a3882636823
abdulhay103 <abdulhay.me@gmail.com>
2023-08-31 23:34:17 +0600

    done

--- src/app/api/about/route.js
+++ src/app/api/about/route.js
@@ -2,8 +2,5 @@ import { NextResponse } from "next/server";
 import { headers } from "next/headers";
 
 export async function GET(req, res) {
-  const allHeaders = headers();
-  let user = allHeaders.get("user");
-  let password = allHeaders.get("password");
-  return NextResponse.json({ user: user, Password: password });
+  return NextResponse.json({ msg: "ok" });
 }
--- src/middleware.js
+++ src/middleware.js
@@ -2,16 +2,10 @@ import { NextResponse } from "next/server";
 
 export function middleware(req, res) {
   if (req.nextUrl.pathname.startsWith("/api/about")) {
-    const reqHeader = new Headers(req.headers);
-    let token = reqHeader.get("token");
-
-    if (token === "123") {
-      reqHeader.set("user", "Abdul Hay");
-      reqHeader.set("password", "1234");
-      return NextResponse.next({ request: { headers: reqHeader } });
-    } else {
-      return NextResponse.json({ msg: "Fail" }, { status: 401 });
-    }
+    const res = NextResponse.next();
+    res.headers.set("key1", "123");
+    res.headers.set("key2", "1234");
+    return res;
   } else if (req.nextUrl.pathname.startsWith("/about")) {
     console.log("about Middle ware");
   }
