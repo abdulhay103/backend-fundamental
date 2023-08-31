b30cbd687f23cec7cd388304d7b33f84c02e2923
abdulhay103 <abdulhay.me@gmail.com>
2023-08-31 10:53:51 +0600

    done

--- src/app/api/about/route.js
+++ src/app/api/about/route.js
@@ -1,13 +1,17 @@
 import { NextResponse } from "next/server";
 
 export async function POST(req, res) {
-  const requestBody = await req.json();
-  let name = requestBody["name"];
-  let age = requestBody["age"];
-  let color = requestBody["color"];
+  const formData = await req.formData();
+  let brand = formData.get("brand");
+  let origin = formData.get("origin");
+  let importer = formData.get("importer");
 
   return NextResponse.json(
-    { Name: name, Age: age, Color: color },
+    {
+      Brand: brand,
+      Origin: origin,
+      Importer: importer,
+    },
     { status: 202 }
   );
 }
